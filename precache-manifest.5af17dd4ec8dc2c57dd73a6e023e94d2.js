4c2c536bf130420303bd233baa3d79564d9b8951
Osama Imadi <osamaimadi@yahoo.com>
2020-07-02 02:07:01 +0500

    Updates

---
+++ precache-manifest.5af17dd4ec8dc2c57dd73a6e023e94d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "e418b257c2e20e823e1d",
+    "url": "/portfolio/static/js/main.e418b257.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "e418b257c2e20e823e1d",
+    "url": "/portfolio/static/css/main.2b4c8498.chunk.css"
+  },
+  {
+    "revision": "96cfa6f06cc0167663c53814a8e75105",
+    "url": "/portfolio/index.html"
+  }
+];
